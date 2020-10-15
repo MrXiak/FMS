21616c9a5c482ec633ca4a60f2c8b60ffdd02cfa
halt081017 <674636920@qq.com>
2020-10-15 11:37:12 +0800

    10/15.01

---
+++ web/admin/tablepage.js
@@ -0,0 +1,55 @@
+var nowPage = 0, //当前页
+count = 5, //每页显示多少条消息
+pageAll = 0; //总页数
+var testDataList = []; //创建一个存放数据的数组
+for (var m = 0; m < 53; m++) { //往数组里放入53条数据
+    testDataList.push(`<tr>
+        <td>第` + m + `条消息</td>
+        <td>第` + m + `条消息</td>
+        <td>第` + m + `条消息</td>
+        <td>第` + m + `条消息</td>
+        <td>第` + m + `条消息</td>
+        <td>第` + m + `条消息</td>
+        <td>第` + m + `条消息</td>
+        <td>
+            <button type="button"  class="btnShow edit"> 编辑</button>
+            <button type="button" class="btnShow delete"> 删除</button>
+        </td>
+    </tr>`);
+    }
+pageAll = (testDataList.length) / 5; //计算总页数
+var setTable = function () { //数据渲染表格
+    var onePageData = []; //用来存放一页的数据
+    /*
+        i + nowPage * count 表示当前是第几条数据
+        (nowPage + 1) * count 表示到下一条数据 之间 有几条数据  即当前页 的页尾数据的是第几条 不是最后一页的情况下都是五条
+         var begin = (nowPage + 1) * count
+         var end = i + nowPage * count)
+         begin-end==5
+
+         testDataList.length 表示所有数据个数
+
+    */
+    for (var i = 0;
+         (i + nowPage * count < (nowPage + 1) * count) && i + nowPage * count < testDataList.length; i++) { //满足当前数据小于没到当前页的最后一条数据 ，并且当前数据没到最后一条数据
+        {
+            onePageData.push(testDataList[i + nowPage * count]);// 这个循环会循环五次  把五条数据放到列表里
+        }
+    }
+    outBoxTab.querySelector('tbody').innerHTML = onePageData.join(''); //渲染当前页数据
+}
+setTable();
+up.onclick = function () { //上一页
+    if (nowPage == 0) //当前页数是第一页则返回
+        return
+    nowPage--;
+    setTable();
+}
+down.onclick = function () {//下一页
+    if (nowPage >= pageAll-1){
+        return
+    } //当前页数是最后一页则返回  这么写是因为总页数不一定是整数
+    nowPage++;
+    setTable();
+    debugger
+}
--- web/js/admin-navigation-head.js
+++ web/js/admin-navigation-head.js
@@ -23,11 +23,11 @@ document.writeln("                <img src=\'http://t.cn/RCzsdCq\' class=\'layui
 document.writeln("                贤心");
 document.writeln("            </a>");
 document.writeln("            <dl class=\'layui-nav-child\'>");
-document.writeln("                <dd><a href=\'\'>基本资料</a></dd>");
-document.writeln("                <dd><a href=\'\'>安全设置</a></dd>");
+document.writeln("                <dd><a href=\'\'>我的订单</a></dd>");
+document.writeln("                <dd><a href=\'\'>基本信息</a></dd>");
+document.writeln("                <dd><a href=\'\'>退出登录</a></dd>");
 document.writeln("            </dl>");
 document.writeln("        </li>");
-document.writeln("        <li class=\'layui-nav-item\'><a href=\'\'>退了</a></li>");
 document.writeln("    </ul>");
 document.writeln("</div>");
 document.writeln("    <script>");
